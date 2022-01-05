830ce15ce0b3b93f46a9f80b1fc4d9c8b380ad53
hayounsong <fishingest@kookmin.ac.kr>
2022-01-05 17:38:09 +0900

    1-9장

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,9 +1,10 @@
+import random
 global num
 num=0
 global player1
-player1="PlayerA"
+player1="player"
 global player2
-player2="PlayerB"
+player2="computer"
 global player
 player=player2
 
@@ -17,28 +18,38 @@ def changeplayer():
     else:
         player = player2
 def brGame():
-    try:
-        global player
-        global num
-        num_input = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if num_input > 3:
-            raise RangeError
-        changeplayer()
-    except ValueError:
-        print("정수를 입력해주세요")
-    except RangeError:
-        print('1,2,3 중 하나를 입력하세요')
-    else:
-        for i in range(0, num_input):
-            num = num + 1
-            print(player + ": " + str(num))
-            if num == 31:
-                break
+    while True:
+        try:
+            global player
+            global num
+
+
+
+            if player=="player":
+                num_input = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            elif player=="computer":
+                num_input=random.randint(1,3)
+            if num_input > 3:
+                raise RangeError
+
+        except ValueError:
+            print("정수를 입력해주세요")
+        except RangeError:
+            print('1,2,3 중 하나를 입력하세요')
+        else:
+            break
+
+    for i in range(0, num_input):
+        num = num + 1
+        print(player + ": " + str(num))
+        if num == 31:
+            break
+    changeplayer()
+
 
 while num<31:
     brGame()
 
-changeplayer()
 
 print(player,"win!")
 
