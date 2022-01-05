a6e6e2036bfc26f0556fa6b1ee7f24fc2cd9ca5a
hayounsong <fishingest@kookmin.ac.kr>
2022-01-05 17:03:41 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -6,7 +6,7 @@ class RangeError(Exception):
     def __init__(self):
         super().__init__('1,2,3 중 하나를 입력하세요')
 
-while True:
+while num!=31:
     try:
 
         num_input=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
