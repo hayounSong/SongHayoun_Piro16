8bc2563e61795527a8ff775db69452120342f618
hayounsong <fishingest@kookmin.ac.kr>
2022-01-13 02:19:30 +0900

    애플리케이션 확장하기

--- djangogirls/djangogirls/mysite/blog/urls.py
+++ djangogirls/djangogirls/mysite/blog/urls.py
@@ -1,8 +1,12 @@
-
 from django.urls import path
 from . import views
-
-urlpatterns=[
-    path('',views.post_list,name='post_list')
-
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    
 ]
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
