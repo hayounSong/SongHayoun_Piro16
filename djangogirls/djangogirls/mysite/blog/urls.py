e96c9ab2dace25c9d4738b49dda6704da6bf78fa
hayounsong <fishingest@kookmin.ac.kr>
2022-01-13 02:30:17 +0900

    Django 폼

--- djangogirls/djangogirls/mysite/blog/urls.py
+++ djangogirls/djangogirls/mysite/blog/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
