8e9becf541b29176dc3b0413640f46d3eb156469
hayounsong <fishingest@kookmin.ac.kr>
2022-01-12 12:27:48 +0900

    Python 시작하기

---
+++ djangogirls/python_intro.py
@@ -0,0 +1,26 @@
+
+if 3>2:
+    print('Hello, Django girls!')
+
+    if 5 > 2:
+        print('5 is indeed greater than 2')
+    else:
+        print('5 is not greater than 2')
+
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
+
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous!')
+
+hi('hayoun')    
